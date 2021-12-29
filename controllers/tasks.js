a51b03952cd2357ac6fd47297fe61908a130e058
Usblankson <uyohoiniblankson11@gmail.com>
2021-12-29 02:26:21 +0100

    added create task functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,18 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
     res.send('All tasks from the list')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
-}
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+
+    }catch (error){
+       res.status(500).json({msg: error})
+    }
+}  
 const getTask = (req, res) => {
     res.json({id: req.params.id})
 }
